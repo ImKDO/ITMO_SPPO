06caa3a66840ea2b10c864daf1ef9538cd4c2c7e
Dmitriy <crav4enkoadmitriy@mail.ru>
2024-10-04 13:23:34 +0300

    TFCV + first 2 lec

--- 2course/WEB/labs/lab1/Back/kt/src/main/kotlin/Main.kt
+++ 2course/WEB/labs/lab1/Back/kt/src/main/kotlin/Main.kt
@@ -1,46 +1,181 @@
 import com.fastcgi.FCGIInterface
-import java.nio.ByteBuffer
+
 import java.nio.charset.StandardCharsets
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.system.measureNanoTime
 
 
 fun main() {
+
+    var beginHTMLresponse = """<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>lab1</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="imgs/title.svg">
+</head>
+
+
+<body>
+<header>
+    <div class="flp header_blocks_class">
+        <p>Кравченко</p>
+        <p>Дмитрий</p>
+        <p>Олегович</p>
+    </div>
+    <div class="group header_blocks_class">
+        <p>Группа: P3219</p>
+    </div>
+    <div class="lab_number header_blocks_class">
+        <p>Вариант: 408891</p>
+    </div>
+</header>
+<img src="imgs/asd.gif" style="width: 25%;">
+<p>Введите данные:</p>
+
+<main>
+    <div></div>
+    <div class="form_for_img">
+        <div class="check_dot grid_form_layout">
+            <div id="x" class="check_dot_block_X">
+                X:
+                <form class="check_dot_blocks">
+                    <input class="styleInput" type="button" value="-2" lang="X_button">
+                    <input class="styleInput" type="button" value="-1" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+                    <input class="styleInput" type="button" value="0" lang="X_button">
+
+                </form>
+            </div>
+            <div id="y">
+                <form class="check_dot_blocks styleInput">
+                    Y:
+                    <input class="styleInput" id="textInput" maxlength="5" type="text">
+                </form>
+            </div>
+            <div class="styleInput">
+                <button id="submitBtn_dot" class="check_dot_blocks styleInput">Проверить точку</button>
+            </div>
+
+        </div>
+        <div class="styleInput">
+            <form id="radius">
+                R:
+                <input type="radio" name="radius" value="10">1
+                <input type="radio" name="radius" value="5">2
+                <input type="radio" name="radius" value="24">3
+            </form>
+            <button onclick="drawGraph()" id="submitBtn" class="styleInput">Отправить данные</button>
+        </div>
+        <div id="draw_img">
+            <canvas id="Canvas" width="500" height="400"></canvas>
+        </div>
+    </div>
+    <div class="resTable">
+        <div class="resTable_blocks">x</div>
+        <div class="resTable_blocks">y</div>
+        <div class="resTable_blocks">R</div>
+        <div class="resTable_blocks">Попал?</div>
+        <div class="resTable_blocks">Время выполнения скрипта</div>
+        <div >Когда был выполнен скрипт</div>
+    </div>
+</main>
+</body>
+<script src="js/main.js"></script>
+</html>"""
+
     var fcgiInterface = FCGIInterface()
     while (fcgiInterface.FCGIaccept() >= 0) {
 
-        var content = """
-                <html>
-                <head><title>Java FastCGI Hello World</title></head>
-                <body><hi>Hello, World!</h1></body>
-                </html>";
-                """
+        // Получаем метод запроса
+        val requestMethod = FCGIInterface.request.params.getProperty("REQUEST_METHOD")
 
-        var readRequest = readRequestBody().also {
-            println(it)
+        // Проверяем, что это GET-запрос
+        if (requestMethod == "GET") {
+            // Получаем параметры из строки запроса
+            val queryString = FCGIInterface.request.params.getProperty("QUERY_STRING")
+            // Преобразуем в объект Date
+            val currentTimeMillis = System.currentTimeMillis()
+
+            val date = Date(currentTimeMillis)
+
+            // Форматируем дату в читаемый формат
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val formattedDate = dateFormat.format(date)
+
+            var content = """
+            {
+            "status":"%s",
+            "x":"%d",
+            "y":"%f",
+            "r":"%f",
+            "hit":"%b",
+            "time_script":"%d",
+            "time_now":"$formattedDate"
+            }
+        """.trimMargin()
+
+            // Обработка параметров
+            val params = queryString?.split("&")?.associate {
+                val (key, value) = it.split("=")
+                key to value
+            }
+            val x = params?.get("x")
+            val y = params?.get("y")
+            val r = params?.get("r")
+            if (validateValuesXYR(x, y, r)) {
+                val xNum = x?.toInt()
+                val yNum = y?.toDouble()
+                val rNum = r?.toDouble()
+                val timeScript = measureNanoTime {
+                    script(xNum!!, yNum!!, rNum!!)
+                }
+//                    System.err.println(script(xNum!!, yNum!!, rNum!!))
+                if (script(xNum!!, yNum!!, rNum!!)) {
+                    // Обновляем переменную content
+                   content = content.format("true",xNum,yNum,rNum,true,timeScript)
+                } else{
+                    content = content.format("false",xNum,yNum,rNum,false,timeScript)
+                }
+            } else{
+                content = content.format()
+            }
+
+
+            var httpResponse = """
+HTTP/1.1 200 OK
+Content-Type: application/json
+Content-Length: %d
+
+%s""".format("content".toByteArray(StandardCharsets.UTF_8).toString().length, content)
+            println(httpResponse)
         }
-
-        var httpResponse =
-            """HTTP/1.1 200 OK
-                Content-Type: text/html
-                Content-Length: %d
-                
-                %s
-""".format(content.toByteArray(StandardCharsets.UTF_8).size, content)
-        println(httpResponse);
     }
-
 }
 
-private fun readRequestBody(): String {
-    FCGIInterface.request.inStream.fill()
-    val contentLength = FCGIInterface.request.inStream.available()
-    val buffer: ByteBuffer = ByteBuffer.allocate(contentLength)
-    val readBytes =
-        FCGIInterface.request.inStream.read(
-            buffer.array(), 0,
-            contentLength
-        )
-    val requestBodyRaw = ByteArray(readBytes)
-    buffer.get(requestBodyRaw)
-    buffer.clear()
-    return String(requestBodyRaw, StandardCharsets.UTF_8)
+
+fun isNumeric(toCheck: String): Boolean {
+    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
+    return toCheck.matches(regex)
+}
+
+fun validateValuesXYR(x: String?, y: String?, r: String?): Boolean {
+    return isNumeric(x.toString()) && isNumeric(y.toString()) && isNumeric(r.toString())
+}
+
+fun script(x: Int, y: Double, r: Double): Boolean {
+    val areaCircle = ((x * x + y * y) <= r * r) && (x <= 0) && (y >= 0)
+    val areaTriangle = (x <= 0) && (y <= 0) && (y >= -x - r)
+    val areaReactangle = (y <= 0) && y <= (-r) && (x >= 0) && (x <= r / 2)
+    return areaCircle || areaTriangle || areaReactangle
 }
--- 2course/WEB/labs/lab1/Front/js/main.js
+++ 2course/WEB/labs/lab1/Front/js/main.js
@@ -1,122 +1,147 @@
+    let x = -5;
+    let canvas = document.getElementById("Canvas")
+    let ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-let x = -5
-let canvas = document.getElementById("Canvas")
-let ctx = canvas.getContext("2d");
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+    function fetchRequest(x,y,r){
+        // Формируем строку параметров для GET-запроса
+        const params = new URLSearchParams({
+            x: x,  // Параметр 'x' со значением переменной x
+            y: y,   // Параметр 'y' со значением переменной y
+            r: r
+        }).toString();
 
-
-let formX = document.getElementById("x")
-formX.querySelectorAll('input[type="button"]').forEach(button => {
-    button.addEventListener('click', (event) => {
-        x = event.target.value
-    })
-})
-
-
-
-document.getElementById("submitBtn_dot").addEventListener('click', (event) => {
-    let y = document.getElementById("textInput").value;
-    console.log(!isNaN(y));
-    console.log(!isNaN(y));
-
-    if(!isNaN(y) && !(y.trim() === '') && x !== -5) {
-        ctx.beginPath();
-        ctx.strokeStyle = "yellow"
-        ctx.lineWidth = 2;
-        ctx.arc(250 + x*50, y*80, 3, 0, 2 * Math.PI);
-        ctx.stroke()
-
+        fetch(`/fcgi-bin/?${params}`)
+            .then(response => {
+                console.log(response);
+            return response.text();
+            })
+            .then(data => {
+            const jsonIndex = data.indexOf('{');
+            const jsonString = data.slice(jsonIndex);
+            data = JSON.parse(jsonString);
+            console.log(data);
+                const status = data.status;
+                alert(status);
+            })
     }
-})
-
-ctx.beginPath();
-ctx.moveTo(250, 200);  // Начинаем рисовать с центра круга
-ctx.arc(250, 200, 100, Math.PI,3*Math.PI/2);  // Рисуем дугу от 0 до 90 градусов
-ctx.lineTo(250, 200);  // Замыкаем сектор линией обратно к центру
-ctx.closePath();
-
-ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
-ctx.fill();  // Закрашиваем сектор
-ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
-
-// ctx.beginPath();  // Начинаем новый путь
-ctx.moveTo(250, 200);  // Перемещаемся к первой вершине треугольника
-ctx.lineTo(250, 310);   // Линия ко второй вершине
-ctx.lineTo(150, 200);  // Линия к третьей вершине
-ctx.closePath();  // Замыкаем путь (соединяет последнюю точку с первой)
-
-ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
-ctx.fill();  // Закрашиваем треугольник
-ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
-
-// ctx.fillStyle = "green";  // Устанавливаем цвет заливки
-ctx.fillRect(250, 200, 100, 110);  // Рисуем прямоугольник с координатами (50, 50), шириной 100 и высотой 150
 
 
-ctx.stroke();  // Рисуем обводку
+    //Получаем значение x
+    let formX = document.getElementById("x")
+    formX.querySelectorAll('input[type="button"]').forEach(button => {
+        button.addEventListener('click', (event) => {
+            x = event.target.value
+        })
+    })
+
+
+    let y = 0
+    document.getElementById("submitBtn_dot").addEventListener('click', (event) => {
+        let y = document.getElementById("textInput").value;
+
+        if (!isNaN(y) && !(y.trim() === '') && x !== -5) {
+            fetchRequest(x,y)
+            ctx.beginPath();
+            ctx.strokeStyle = "yellow";
+            ctx.lineWidth = 2;
+            ctx.arc(250 + x * 50, 250 + y * 80, 3, 0, 2 * Math.PI);
+            ctx.stroke()
+        }
+    })
 
 
 
 
-ctx.stroke();  // Рисуем обводку
-
-// Рисуем закрашенный прямоугольник
-ctx.beginPath();
 
 
 
-// Настройка стиля
 
-// Нарисуем оси X и Y
-ctx.beginPath();
-ctx.strokeStyle = "black"; // цвет линии
-ctx.lineWidth = 2;        // толщина линии
+    ctx.beginPath();
+    ctx.moveTo(250, 200);  // Начинаем рисовать с центра круга
+    ctx.arc(250, 200, 100, Math.PI, 3 * Math.PI / 2);  // Рисуем дугу от 0 до 90 градусов
+    ctx.lineTo(250, 200);  // Замыкаем сектор линией обратно к центру
+    ctx.closePath();
+
+    ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
+    ctx.fill();  // Закрашиваем сектор
+    ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
+
+    // ctx.beginPath();  // Начинаем новый путь
+    ctx.moveTo(250, 200);  // Перемещаемся к первой вершине треугольника
+    ctx.lineTo(250, 310);   // Линия ко второй вершине
+    ctx.lineTo(150, 200);  // Линия к третьей вершине
+    ctx.closePath();  // Замыкаем путь (соединяет последнюю точку с первой)
+
+    ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
+    ctx.fill();  // Закрашиваем треугольник
+    ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
+
+    // ctx.fillStyle = "green";  // Устанавливаем цвет заливки
+    ctx.fillRect(250, 200, 100, 110);  // Рисуем прямоугольник с координатами (50, 50), шириной 100 и высотой 150
 
 
-ctx.moveTo(250, 0); // Начало линии
-ctx.lineTo(250, 500); // Ось Y11
+    ctx.stroke();  // Рисуем обводку
 
-ctx.moveTo(0, 200); // Начало линии
-ctx.lineTo(500, 200); // Ось X
 
-ctx.stroke();
+    ctx.stroke();  // Рисуем обводку
 
-ctx.beginPath();
-ctx.strokeStyle = "red"; // цвет линии
+    // Рисуем закрашенный прямоугольник
+    ctx.beginPath();
 
-ctx.font = '16px Arial'; // Устанавливаем шрифт и размер
-ctx.fillStyle = 'red'; // Устанавливаем цвет текста
-ctx.fillText('R/2', 280, 155); // Рисуем текст рядом с линией
 
-// Рисуем деление для оси
+    // Настройка стиля
 
-//Вертикальныая ось ----------
-ctx.moveTo(240, 150);
-ctx.lineTo(260, 150);
+    // Нарисуем оси X и Y
+    ctx.beginPath();
+    ctx.strokeStyle = "black"; // цвет линии
+    ctx.lineWidth = 2;        // толщина линии
 
-ctx.moveTo(240, 250);
-ctx.lineTo(260, 250);
 
-ctx.moveTo(200, 190);
-ctx.lineTo(200, 210);
+    ctx.moveTo(250, 0); // Начало линии
+    ctx.lineTo(250, 500); // Ось Y11
 
-ctx.moveTo(300, 190);
-ctx.lineTo(300, 210);
-//Вертикальныая ось ----------
+    ctx.moveTo(0, 200); // Начало линии
+    ctx.lineTo(500, 200); // Ось X
 
-//Горизонтальная ось ---------
-ctx.moveTo(240, 100);
-ctx.lineTo(260, 100);
+    ctx.stroke();
 
-ctx.moveTo(240, 310);
-ctx.lineTo(260, 310);
+    ctx.beginPath();
+    ctx.strokeStyle = "red"; // цвет линии
 
-ctx.moveTo(150, 190);
-ctx.lineTo(150, 210);
+    ctx.font = '16px Arial'; // Устанавливаем шрифт и размер
+    ctx.fillStyle = 'red'; // Устанавливаем цвет текста
+    ctx.fillText('R/2', 280, 155); // Рисуем текст рядом с линией
 
-ctx.moveTo(350, 190);
-ctx.lineTo(350, 210);
-//Горизонтальная ось ---------
-ctx.stroke()
+    // Рисуем деление для оси
+
+    //Вертикальныая ось ----------
+    ctx.moveTo(240, 150);
+    ctx.lineTo(260, 150);
+
+    ctx.moveTo(240, 250);
+    ctx.lineTo(260, 250);
+
+    ctx.moveTo(200, 190);
+    ctx.lineTo(200, 210);
+
+    ctx.moveTo(300, 190);
+    ctx.lineTo(300, 210);
+    //Вертикальныая ось ----------
+
+    //Горизонтальная ось ---------
+    ctx.moveTo(240, 100);
+    ctx.lineTo(260, 100);
+
+    ctx.moveTo(240, 310);
+    ctx.lineTo(260, 310);
+
+    ctx.moveTo(150, 190);
+    ctx.lineTo(150, 210);
+
+    ctx.moveTo(350, 190);
+    ctx.lineTo(350, 210);
+    //Горизонтальная ось ---------
+    ctx.stroke()
 
 
