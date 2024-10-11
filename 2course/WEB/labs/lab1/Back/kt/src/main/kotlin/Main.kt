c4b37ab4e4ac434cea70b3466916e25b7a0330c5
Dmitriy <crav4enkoadmitriy@mail.ru>
2024-10-12 01:02:44 +0300

    fix 1 lab http 200 & valid data

--- 2course/WEB/labs/lab1/Back/kt/src/main/kotlin/Main.kt
+++ 2course/WEB/labs/lab1/Back/kt/src/main/kotlin/Main.kt
@@ -8,62 +8,63 @@ import kotlin.system.measureNanoTime
 fun main() {
     var fcgiInterface = FCGIInterface()
     while (fcgiInterface.FCGIaccept() >= 0) {
-        try {
+        // Получаем метод запроса
+        val requestMethod = FCGIInterface.request.params.getProperty("REQUEST_METHOD")
+        var content: String
 
-            // Получаем метод запроса
-            val requestMethod = FCGIInterface.request.params.getProperty("REQUEST_METHOD")
-            var content: String
+        val start = System.nanoTime()
 
-            val start = System.nanoTime()
-
-            // Проверяем, что это GET-запрос
-            if (requestMethod == "GET") {
+        // Проверяем, что это GET-запрос
+        if (requestMethod == "GET") {
 
 
-                // Получаем параметры из строки запроса
-                val queryString = FCGIInterface.request.params.getProperty("QUERY_STRING")
-                if (queryString != null) {
+            // Получаем параметры из строки запроса
+            val queryString = FCGIInterface.request.params.getProperty("QUERY_STRING")
+            if (queryString != null) {
 
-                    // Преобразуем в объект Date
-                    val currentTimeMillis = System.currentTimeMillis()
+                // Преобразуем в объект Date
+                val currentTimeMillis = System.currentTimeMillis()
 
-                    // Обработка параметров
-                    val params: HashMap<String, String> = getValues(queryString)
-                    val x = params["x"].toString()
-                    val y = params["y"].toString()
-                    val r = params["r"].toString()
+                // Обработка параметров
+                val params: HashMap<String, String> = getValues(queryString)
+                val x = params["x"].toString()
+                val y = params["y"].toString()
+                val r = params["r"].toString()
 
-                    try {
-                        if (script(x.toInt(), y.toFloat(), r.toFloat()) && validateValuesXYR(x, y, r)) {
+                try {
+                    if(validateValuesXYR(x, y, r)){
+                        if (script(x.toInt(), y.toFloat(), r.toFloat())) {
                             content = response(true, x, y, r, start)
                         } else {
                             content = response(false, x, y, r, start)
                         }
                         println(content)
-                    } catch (e: Exception) {
-                        println(errorResponse("Not valid data"))
+                    } else {
+                     println(errorResponse("Not valid data"))
                     }
-                } else {
-                    println(errorResponse("Query is null"))
+                } catch (e: Exception) {
+                    println(errorResponse("Not valid data"))
                 }
             } else {
-                println(errorResponse("Not valid http method"))
+                println(errorResponse("Query is null"))
             }
-        } catch (e: Exception) {
-            println(errorResponse("kpjj[fkjfdkl;aklfaklaflk;asf"))
+        } else {
+            println(errorResponse("Not valid http method"))
         }
-
     }
 }
 
-
-fun isNumeric(toCheck: String): Boolean {
-    val regex = "-?[0-9]+(\\.[0-9]+)?".toRegex()
-    return toCheck.matches(regex)
-}
-
 fun validateValuesXYR(x: String?, y: String?, r: String?): Boolean {
-    return x?.toInt() != 60 && -3 <= y?.toFloat()!! && y.toFloat() <= 5 && isNumeric(r.toString())
+    val xNum = x?.toInt()!!
+    val yNum = y?.toFloat()!!
+    val rNum = r?.toFloat()
+    return (intArrayOf(-3, -2, -1, 0, 1, 2, 3, 4, 5).contains(xNum)) && (-3 <= yNum) && (yNum <= 5) && (arrayOf(
+        1f,
+        1.5f,
+        2f,
+        2.5f,
+        3f
+    ).contains(rNum))
 }
 
 fun script(x: Int, y: Float, r: Float): Boolean {
@@ -86,7 +87,7 @@ private fun getValues(inpString: String): LinkedHashMap<String, String> {
 private fun response(isHit: Boolean, x: String, y: String, r: String, startTime: Long): String {
     val endTime = System.nanoTime()
     val duration = (endTime - startTime)
-    val scrTime = "$duration нс" // в наносекундах
+    val scrTime = "$duration" // в наносекундах
 
     val sdf = SimpleDateFormat("dd.MM.yy HH:mm:ss")
     val currentDateTime = sdf.format(Date())
@@ -95,6 +96,7 @@ private fun response(isHit: Boolean, x: String, y: String, r: String, startTime:
                 
                 """.trimIndent().format(isHit, x, y, r, currentDateTime, scrTime)
     return """
+                HTTP/1.1 200 OK
                 Content-Type: application/json
                 Content-Length: %d
                 
--- 2course/WEB/labs/lab1/Front/js/main.js
+++ 2course/WEB/labs/lab1/Front/js/main.js
@@ -1,147 +1,203 @@
-    let x = -5;
-    let canvas = document.getElementById("Canvas")
-    let ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+let x = 60;
+let y = 0;
+let r = 60
 
-    function fetchRequest(x,y,r){
-        // Формируем строку параметров для GET-запроса
-        const params = new URLSearchParams({
-            x: x,  // Параметр 'x' со значением переменной x
-            y: y,   // Параметр 'y' со значением переменной y
-            r: r
-        }).toString();
+let canvas = document.getElementById("Canvas")
+let ctx = canvas.getContext("2d");
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        fetch(`/fcgi-bin/?${params}`)
-            .then(response => {
-                console.log(response);
+
+//--------------------------------------------------------
+// Функция для добавления новой записи в таблицу
+function addResultRow(x, y, r, hit, timeScript, nowTime) {
+    const resultTableBody = document.getElementById('resultTableBody');
+
+    // Создаем новый элемент строки таблицы
+    const row = document.createElement('tr');
+
+    // Добавляем значения в ячейки
+    row.innerHTML = `
+        <td>${x}</td>
+        <td>${y}</td>
+        <td>${r}</td>
+        <td>${hit}</td>
+        <td>${timeScript} нс</td>
+        <td>${nowTime}</td>
+    `;
+
+    // Добавляем новую строку в тело таблицы
+    resultTableBody.appendChild(row);
+}
+
+//--------------------------------------------------------
+
+//Функция fetch запроса с ответом
+//--------------------------------------------------------
+function fetchRequest(x,y,r){
+    // Формируем строку параметров для GET-запроса
+    const params = new URLSearchParams({
+        x: x,  // Параметр 'x' со значением переменной x
+        y: y,   // Параметр 'y' со значением переменной y   
+        r: r
+    }).toString();
+
+
+
+
+    fetch(`/fcgi-bin/?x=${x}&y=${y}&r=${r}`, {
+        method: 'GET'
+    })
+
+        .then(response => {
+            console.log(response)
+            if (!response.ok) {
+                throw new Error(`${response.status}`);
+            }
             return response.text();
-            })
-            .then(data => {
-            const jsonIndex = data.indexOf('{');
-            const jsonString = data.slice(jsonIndex);
-            data = JSON.parse(jsonString);
-            console.log(data);
-                const status = data.status;
-                alert(status);
-            })
-    }
-
-
-    //Получаем значение x
-    let formX = document.getElementById("x")
-    formX.querySelectorAll('input[type="button"]').forEach(button => {
-        button.addEventListener('click', (event) => {
-            x = event.target.value
         })
+
+        .then(answer => {
+            let resultParse = JSON.parse(answer);
+            const xRes = resultParse.x
+            const yRes = resultParse.y
+            const rRes = resultParse.r
+            const result = resultParse.result
+            const scrTime = resultParse.scrTime
+            const currentTime = resultParse.currTime
+            const error = resultParse.error
+            addResultRow(xRes,yRes,rRes,result,scrTime, currentTime)
+
+        })
+}
+
+
+
+//--------------------------------------------------------
+
+
+//Получаем значение x
+//--------------------------------------------------------
+let formX = document.getElementById("x")
+formX.querySelectorAll('input[type="button"]').forEach(button => {
+    button.addEventListener('click', (event) => {
+        x = event.target.value
     })
-
-
-    let y = 0
-    document.getElementById("submitBtn_dot").addEventListener('click', (event) => {
-        let y = document.getElementById("textInput").value;
-
-        if (!isNaN(y) && !(y.trim() === '') && x !== -5) {
-            fetchRequest(x,y)
-            ctx.beginPath();
-            ctx.strokeStyle = "yellow";
-            ctx.lineWidth = 2;
-            ctx.arc(250 + x * 50, 250 + y * 80, 3, 0, 2 * Math.PI);
-            ctx.stroke()
-        }
+})
+//Получаем значение r
+let formR = document.getElementById("radius")
+formR.querySelectorAll('input[type="radio"]').forEach(button => {
+    button.addEventListener('click', (event) => {
+        r = event.target.value
     })
+})
+//--------------------------------------------------------
+
+//--------------------------------------------------------
+document.getElementById("submitBtn_dot").addEventListener('click', (event) => {
+    y = document.getElementById("textInput").value;
+
+    if (!isNaN(y) && !(y.trim() === '') && x !== 60 && r != 60) {
+        ctx.beginPath();
+        ctx.strokeStyle = "yellow";
+        ctx.lineWidth = 2;
+        ctx.arc(250 + x * 50, 200 - y * 33, 3, 0, 2 * Math.PI);
+        ctx.stroke()
+    } else{
+        alert("Введите валидные")
+    }
+})
+//--------------------------------------------------------
+document.getElementById("submitBtn").addEventListener('click', (event) => {
+    fetchRequest(x,y,r)
+})
 
 
 
 
+// Отрисовка картинки
+ctx.beginPath();
+ctx.moveTo(250, 200);  // Начинаем рисовать с центра круга
+ctx.arc(250, 200, 100, Math.PI, 3 * Math.PI / 2);  // Рисуем дугу от 0 до 90 градусов
+ctx.lineTo(250, 200);  // Замыкаем сектор линией обратно к центру
+ctx.closePath();
+
+ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
+ctx.fill();  // Закрашиваем сектор
+ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
+
+// ctx.beginPath();  // Начинаем новый путь
+ctx.moveTo(250, 200);  // Перемещаемся к первой вершине треугольника
+ctx.lineTo(250, 310);   // Линия ко второй вершине
+ctx.lineTo(150, 200);  // Линия к третьей вершине
+ctx.closePath();  // Замыкаем путь (соединяет последнюю точку с первой)
+
+ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
+ctx.fill();  // Закрашиваем треугольник
+ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
+
+// ctx.fillStyle = "green";  // Устанавливаем цвет заливки
+ctx.fillRect(250, 200, 100, 110);  // Рисуем прямоугольник с координатами (50, 50), шириной 100 и высотой 150
 
 
+ctx.stroke();  // Рисуем обводку
 
 
-    ctx.beginPath();
-    ctx.moveTo(250, 200);  // Начинаем рисовать с центра круга
-    ctx.arc(250, 200, 100, Math.PI, 3 * Math.PI / 2);  // Рисуем дугу от 0 до 90 градусов
-    ctx.lineTo(250, 200);  // Замыкаем сектор линией обратно к центру
-    ctx.closePath();
+ctx.stroke();  // Рисуем обводку
 
-    ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
-    ctx.fill();  // Закрашиваем сектор
-    ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
-
-    // ctx.beginPath();  // Начинаем новый путь
-    ctx.moveTo(250, 200);  // Перемещаемся к первой вершине треугольника
-    ctx.lineTo(250, 310);   // Линия ко второй вершине
-    ctx.lineTo(150, 200);  // Линия к третьей вершине
-    ctx.closePath();  // Замыкаем путь (соединяет последнюю точку с первой)
-
-    ctx.fillStyle = "blue";  // Устанавливаем цвет заливки
-    ctx.fill();  // Закрашиваем треугольник
-    ctx.strokeStyle = "black";  // Устанавливаем цвет обводки
-
-    // ctx.fillStyle = "green";  // Устанавливаем цвет заливки
-    ctx.fillRect(250, 200, 100, 110);  // Рисуем прямоугольник с координатами (50, 50), шириной 100 и высотой 150
+// Рисуем закрашенный прямоугольник
+ctx.beginPath();
 
 
-    ctx.stroke();  // Рисуем обводку
+// Настройка стиля
+
+// Нарисуем оси X и Y
+ctx.beginPath();
+ctx.strokeStyle = "black"; // цвет линии
+ctx.lineWidth = 2;        // толщина линии
 
 
-    ctx.stroke();  // Рисуем обводку
+ctx.moveTo(250, 0); // Начало линии
+ctx.lineTo(250, 500); // Ось Y11
 
-    // Рисуем закрашенный прямоугольник
-    ctx.beginPath();
+ctx.moveTo(0, 200); // Начало линии
+ctx.lineTo(500, 200); // Ось X
 
+ctx.stroke();
 
-    // Настройка стиля
+ctx.beginPath();
+ctx.strokeStyle = "red"; // цвет линии
 
-    // Нарисуем оси X и Y
-    ctx.beginPath();
-    ctx.strokeStyle = "black"; // цвет линии
-    ctx.lineWidth = 2;        // толщина линии
+ctx.font = '16px Arial'; // Устанавливаем шрифт и размер
+ctx.fillStyle = 'red'; // Устанавливаем цвет текста
+ctx.fillText('R/2', 280, 155); // Рисуем текст рядом с линией
 
+// Рисуем деление для оси
 
-    ctx.moveTo(250, 0); // Начало линии
-    ctx.lineTo(250, 500); // Ось Y11
+//Вертикальныая ось ----------
+ctx.moveTo(240, 150);
+ctx.lineTo(260, 150);
 
-    ctx.moveTo(0, 200); // Начало линии
-    ctx.lineTo(500, 200); // Ось X
+ctx.moveTo(240, 250);
+ctx.lineTo(260, 250);
 
-    ctx.stroke();
+ctx.moveTo(200, 190);
+ctx.lineTo(200, 210);
 
-    ctx.beginPath();
-    ctx.strokeStyle = "red"; // цвет линии
+ctx.moveTo(300, 190);
+ctx.lineTo(300, 210);
+//Вертикальныая ось ----------
 
-    ctx.font = '16px Arial'; // Устанавливаем шрифт и размер
-    ctx.fillStyle = 'red'; // Устанавливаем цвет текста
-    ctx.fillText('R/2', 280, 155); // Рисуем текст рядом с линией
+//Горизонтальная ось ---------
+ctx.moveTo(240, 100);
+ctx.lineTo(260, 100);
 
-    // Рисуем деление для оси
+ctx.moveTo(240, 310);
+ctx.lineTo(260, 310);
 
-    //Вертикальныая ось ----------
-    ctx.moveTo(240, 150);
-    ctx.lineTo(260, 150);
+ctx.moveTo(150, 190);
+ctx.lineTo(150, 210);
 
-    ctx.moveTo(240, 250);
-    ctx.lineTo(260, 250);
-
-    ctx.moveTo(200, 190);
-    ctx.lineTo(200, 210);
-
-    ctx.moveTo(300, 190);
-    ctx.lineTo(300, 210);
-    //Вертикальныая ось ----------
-
-    //Горизонтальная ось ---------
-    ctx.moveTo(240, 100);
-    ctx.lineTo(260, 100);
-
-    ctx.moveTo(240, 310);
-    ctx.lineTo(260, 310);
-
-    ctx.moveTo(150, 190);
-    ctx.lineTo(150, 210);
-
-    ctx.moveTo(350, 190);
-    ctx.lineTo(350, 210);
-    //Горизонтальная ось ---------
-    ctx.stroke()
-
-
+ctx.moveTo(350, 190);
+ctx.lineTo(350, 210);
+//Горизонтальная ось ---------
+ctx.stroke();
